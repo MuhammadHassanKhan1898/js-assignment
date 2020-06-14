b17fe3a41a1e2fdafc9a411077e5f921f89d48ec
MuhammadHassanKhan1898 <hassankhan1898@gmail.com>
2020-06-14 17:27:58 +0500

    js assignment

---
+++ app.js
@@ -0,0 +1,514 @@
+           //Chapter #1. Start
+
+
+
+//alert("Error! Please enter a valid password.");
+
+
+//alert("Welcome to JS Land..\n Happy Coading!");
+
+//var a = "Hello...I can run JS through my web browser's console";
+//console.log(a);
+
+
+             //Chapter #1 end
+
+
+
+
+             //Chapter #2 Start
+
+    // 1. Declare a veriable called username.
+    
+    var username 
+
+    // 2. Declare a veriable called myName & assign to it a string that represent your full name.
+
+    var myName = "Muhammad Hassan Khan";
+
+
+
+    /* 3. write script to
+
+       (a) declare a JS variable, title message.
+       (b) assign "Hello World" to variable message.
+       (c) Display the message in alert box.*/
+
+       
+        //code Start..
+
+      // var message ;
+      // message = "Hello World";
+       //alert(message);
+
+
+       //code end..
+
+
+
+       /* 4. Write a script to save student's bio data in JS variable and show tha data in 
+       alert box.*/
+
+       //code start ..
+
+       /* var firstname = "Muhammad Hassan"
+       alert (firstname);
+
+       var age = "21 year old"
+       alert (age);
+
+
+       var qualification ="Certified Mobile Application Development"
+       alert (qualification);*/
+
+           // code end..
+
+
+/* 6. Declare a variable called email and assing to it a string 
+that represent your email address (e.g. example@example.com).
+show the blow mentioned message in an alert box.(Hint: use string concatenation)*/
+
+//code start
+
+          // var email = "My email address is "
+
+          // var myEmail ="hassankhan123@gmail.com"
+
+          // var a = email + myEmail;
+
+         //  alert(a);
+
+           //code end
+
+
+
+           /* 7. Declare a veriable called book & give it the value "A smarter way to
+            learn JavaScript". Display the following message in alert box.*/
+
+            //code start..
+
+           // var book ="I am trying to learn from the Book A smarter way to learn JavaScript";
+
+           // alert (book);
+
+            //code end..
+
+
+            // 8. Write a script to display this in browser through JS
+
+            //code Start..
+
+
+//var a ="Yah! I can write HTML content through JavaScript"
+
+//            document.write(a);
+
+
+             //code end..
+
+
+
+
+
+           //Chapter #3..
+
+           /* 1. Declare a variable caalled age and assign to it your age.
+           show your age in an alert box.*/
+
+       //code start..
+
+       //var age = "I am 21 years old";
+       //alert(age);
+
+
+       //code end..
+
+
+
+       /* 2. Declare and initialize a veriable to keep track of how many times a
+       visitor has visited a web page. show his/her number of visits
+       on your web page. For example: "You have visited this site N times".*/
+
+       //code Start..
+
+       //var a ="You have visited this site ";
+       //var b =14;
+       //var c = " times"
+       //var abc= a + b + c;
+       // alert(abc);
+
+       //code end
+
+
+
+       /* 3. Declare a veriable called birthyear & assign to it your 
+       birth year. show the following message in your browser: */
+
+       //code start..
+
+
+       //var birthyear = 1998;
+       //document.write("my birth year is ");
+       //document.write(birthyear);
+       //document.write("<br>");
+       //document.write("Data type of my declared variable is " )
+       //document.write(typeof birthyear);
+
+       //code end..
+
+
+
+
+
+       //Chapter #5
+
+
+       /* Write a program that take two numbers and add them in a new variable.
+       show the result in your browser.
+
+
+       //Addition
+       var a = 3;
+       var b = 5;
+       var c = a + b;
+       document.write("sum of 3 and 5 is ");
+       document.write(c);
+     document.write("<br>")
+
+
+       //subtraction
+
+       var a = 3;
+       var b = 5;
+       var c = a - b;
+       document.write("subtraction of 3 and 5 is ");
+       document.write(c);
+       document.write("<br>")
+
+
+       //Multipilication
+
+
+       var a = 3;
+       var b = 5;
+       var c = a * b;
+       document.write("Multipication of 3 and 5 is ");
+       document.write(c);
+       document.write("<br>")
+
+
+       //Division
+
+       var a = 3;
+       var b = 5;
+       var c = a / b;
+       document.write("Division of 3 and 5 is ");
+       document.write(c);
+       document.write("<br>")
+
+       //modulus
+
+       var a = 3;
+       var b = 5;
+       var c = a - b;
+       document.write("modulus of 3 and 5 is ");
+       document.write(c);
+       document.write("<br>")
+
+       */
+
+
+                 //  task no 3   
+
+
+                //code start  
+
+             // Declare a veriable
+
+                        //  var first ;
+
+            // Show the value of veriable after variable declaration...
+
+                       //    document.write("value after variable declaration is " + first);
+
+           // Initialize the variable with some number.
+
+                          //    var first = 5;\
+
+
+          // Show the initial value of veriable..
+          
+                        //      document.write("Initial value : " + first );
+
+
+          // Increment the variable.
+
+                      //      first++ ;
+
+          // Show the value of variable  in browser..
+
+
+                    //    document.write("Value after increment is : " + first);
+
+        // Add 7 to the variable...
+
+                   //   first = first + 7;
+
+
+       //  Show the value after addition
+
+                  //   document.write("Value after addition is : " + first);
+
+
+       // Decrement the variable..
+
+                 //   first-- ;
+
+
+      //  Show the value after decrement
+
+                 //       document.write("Value after decrement is : " + first);
+
+
+
+      // Show the  remainder after dividing the variable's value bye 3 ...
+
+               //   first =    first % 3 ;
+
+
+    // output after division...
+
+               //       document.write("the remainder is : " + first);
+
+
+               // code end...
+
+
+
+               // Task number 4 ...
+
+
+  /* Cost of one movie ticket is 600 PKR. Write a script to store
+     ticket price in a veriable and calculate the cost of buying 5 tickets to a movie */
+
+            //  var ticket = 600;
+
+           //  ticket = ticket * 5;
+
+          //  document.write("total cost to buy 5 tickets to a movie is " + ticket);
+
+
+
+
+          // Task number 5..
+
+  // Display multipication table of any number ..
+
+
+       //    document.write("Table of 2 " + " <br>" );
+
+       //   var a , b ;
+        //  b = 2;
+       //   for(a=1;a<=10;a++)
+
+        //  document.write( b + " x " + a + " = " + b*a + "<br>" );
+
+
+
+               // Task number 6....
+
+  // celsius to fahrenheit...
+
+
+        //   var c =  20;
+          // document.write("Fahrenheit value is: " + (c*9/5+32) + "<br>" );
+
+
+// fahrenheit to celsius.....
+
+      // var f = 68;
+      // document.write("celsius value is : " + (f-32)*5/9);
+
+
+
+           //  Chapter # 6 to 9 .......
+
+  // Task 1
+  //Write a program to take a number in a veriable, do the requried arithmetic to display the following result...
+
+
+       //          var a = 10;
+
+       //     document.write("The value of a is: " + a + "<br>");
+
+       //     ++a ;
+
+        //  document.write("The value of ++a is: " + a + "<br>");
+
+      //    document.write("Now the value of a is: " + a + "<br>");
+
+
+        //   a++ ;
+         
+      //      document.write("The value of a++ is: " + a + "<br>");
+
+            
+      //       document.write("Now the value of a is: " + a + "<br>");
+
+         // --a ;
+
+      //        document.write("The value of --a is: " + a + "<br>");
+
+         //    document.write("Now the value of a is: " + a + "<br>");
+
+
+        //     a-- ;
+
+       //      document.write("The value of a-- is: " + a + "<br>");
+
+       //      document.write("Now the value of a is: " + a + "<br>");
+
+
+
+       //     Task number 2 ...
+
+       //What will be the output in veriables a, b & result after execution of the following script.
+       // var a =2 , b= 1;
+       // var result = --a - --b + ++b + b-- ;
+
+     //  var a = 2 , b = 1;
+      //  var result = --a - --b + ++b + b-- ;
+       //  document.write(--a + "<br>");
+       //  document.write(--a - --b  + "<br>");
+       //  document.write(--a - --b + ++b  + "<br>");
+      //  document.write(--a - --b + ++b + b--  + "<br>");
+
+
+           //     Task number 4
+
+
+       //    var a = prompt("Write your name.");
+
+        //    document.write("WelCome! " + a);
+
+
+
+
+                   // Task number 5 
+
+      // Multiplication Table take a number form user and display its multipication table...
+         
+                //      var a;
+           //        var n = +prompt("Which Table do you want to read?");
+
+            //    document.write("Table of " + n + "<br>");
+
+            //     for(a = 1 ; a<=10; a++)
+
+              //     document.write(n + " x " + a + " = " + n*a + "<br>");
+
+
+
+                    // Chapter 9 to 11 ....
+
+                    // Task 1
+
+  //write a program to take "city" name as input from user. If user enter karachi
+  //welcome the user like this: welcome to city of lights..
+
+       /*     var city = prompt("Enter your city name.");
+
+           if(city == "karachi"){
+             alert("welcome to city of lights")
+           }
+           else if (city == "lahore"){
+             alert("The city of Gardens")
+           }
+           else{
+             alert("Enter Correct name!")
+           }
+
+           */
+
+
+
+
+         // Task number 2
+  
+      /*    var gender = prompt("Enter your Gender")
+          if(gender == "male" ){
+            alert("Good Morning! Sir.")
+          }
+          else if (gender == "female"){
+            alert("Good morning! ma'am")
+          }
+          else{
+            alert("Enter correct Gender")
+          }
+          */
+
+        
+
+          // Task number 4 
+    
+
+         /*  var fuel = prompt("remaining fuel in car")
+          if(fuel  < "1litres"){
+            alert("Please refill the fuel in your car")
+          }
+          else{
+            alert("Don't worry! you have a fuel in your car")
+          }   
+          */
+
+
+          //   Task number 5 
+
+      // a 
+
+     /*   var a = 4;
+      if(++a === 5){
+        alert("given condition for variable a is true");
+      }  */    //This condition is true
+
+
+      //  b
+
+      /*  var b = 82;
+           if(b++ === 83){
+          alert("given condition for variable b is true");
+     }*/         //This condition is false
+
+
+          // e
+
+         /*   if (true){
+            alert("True");
+          }
+          if (false){
+            alert("False");
+          } */    // This condition is False...,
+
+
+
+
+          // Task number 10
+
+/*
+          var temp = +prompt("Enter temperature")
+          if(temp >= 40){
+            alert("it is too hot outside.")
+          }
+          else if(temp >= 30){
+            alert("The Weather today is normal.")
+          }
+          else if(temp >= 20){
+            alert("Today's Weather is cool.")
+          }
+
+          else if (temp >= 10){
+            alert("OMG! Today's Weather is so cool")
+          }
+
+               */
+
